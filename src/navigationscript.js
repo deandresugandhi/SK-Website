13b16a8cac8f3584d28977a30f3b32e2f9804df6
deandresugandhi <deandre_21@hotmail.com>
2024-01-11 21:21:18 +1100

    Added some scrolling effect

--- src/navigationscript.js
+++ src/navigationscript.js
@@ -1,14 +1,28 @@
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobilemenu");
 const navBlock = document.querySelector(".navblock");
+const skicon = document.querySelector(".skicon");
 const nav = document.querySelector("nav");
 const body = document.body;
+const menuLinks = document.querySelectorAll(".mobilemenu a")
 
 navBlock.addEventListener("click", menuClicked);
+menuLinks.forEach(link => {
+    link.addEventListener("click", linkClicked);
+});
 
 function menuClicked() {
     hamburger.classList.toggle("active");
     mobileMenu.classList.toggle("active");
     navBlock.classList.toggle("active");
+    skicon.classList.toggle("active");
     nav.classList.toggle("active");
+}
+
+function linkClicked() {
+    hamburger.classList.remove("active");
+    mobileMenu.classList.remove("active");
+    navBlock.classList.remove("active");
+    skicon.classList.toggle("active");
+    nav.classList.remove("active");
 }
---
+++ src/scrollnavscript.js
@@ -0,0 +1,49 @@
+document.querySelectorAll('a[href^="."]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetClassName = this.getAttribute('href').substring(1);
+        const targetElement = document.querySelector(`.${targetClassName}`);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+function scrollToContent(target) {
+    const targetElement = document.querySelector(target);
+
+    if (targetElement) {
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    } else {
+        console.error(`Element with selector '${target}' not found.`);
+    }
+}
+
+const logo = document.querySelector('.skicon');
+const hamburgerIcon = document.querySelector('.hamburgericon');
+
+window.addEventListener('scroll', function () {
+    const viewportHeight = window.innerHeight;
+    const scrollThresholdPercentage = 50;
+    const scrollThreshold = (scrollThresholdPercentage / 100) * viewportHeight;
+    const scrollThresholdPercentage2 = 190;
+    const scrollThreshold2 = (scrollThresholdPercentage2 / 100) * viewportHeight;
+    
+    if (window.scrollY > scrollThreshold) {
+        logo.classList.add('hidden');
+    } else {
+        logo.classList.remove('hidden');
+    }
+
+    if (window.scrollY > scrollThreshold2) {
+        hamburgerIcon.classList.add('addbackground');
+    } else {
+        hamburgerIcon.classList.remove('addbackground');
+    }
+});
