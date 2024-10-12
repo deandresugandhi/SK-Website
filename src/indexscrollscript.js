36aca1a7a4a3d80180760c7e56d19a3a33b741b3
deandresugandhi <deandre_21@hotmail.com>
2024-10-12 17:45:18 +1100

    Updated work page

--- src/indexscrollscript.js
+++ src/indexscrollscript.js
@@ -2,7 +2,7 @@
 const logo = document.querySelector('.skicon');
 const hamburgerIcon = document.querySelector('.hamburgericon');
 const introParagraph = document.querySelector('#introduction-container p');
-const introLearnMore = document.querySelector('#introduction-container h2:nth-of-type(2)');
+const introLearnMore = document.querySelector('#introduction-container a h2');
 
 window.addEventListener('scroll', function () {
     const viewportHeight = window.innerHeight;
